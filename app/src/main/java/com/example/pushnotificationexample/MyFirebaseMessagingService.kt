2b69374def08368162de34ba7b400cc3d85431dd
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-08-15 05:41:44 +0900

    CREATE : Create Firebase Messaging Service Class

---
+++ app/src/main/java/com/example/pushnotificationexample/MyFirebaseMessagingService.kt
@@ -0,0 +1,16 @@
+package com.example.pushnotificationexample
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+    }
+}
